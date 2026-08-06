2a40ad53a5583ec413f9bf5c47b39ea74f74d5dc
Eyronne <pasadill211@gmail.com>
2026-08-06 20:35:12 +0800

    feat: Add onAdvanceItem prop to QueueCard for item advancement functionality

--- src/components/QueueList.tsx
+++ src/components/QueueList.tsx
@@ -9,11 +9,13 @@ export function QueueList({
   onComplete,
   onVoid,
   onAddItems,
+  onAdvanceItem,
 }: {
   tickets: QueueEntry[];
   onComplete: (id: string) => void;
   onVoid: (id: string) => void;
   onAddItems: (id: string) => void;
+  onAdvanceItem: (orderItemId: string) => void;
 }) {
   if (tickets.length === 0) {
     return (
@@ -26,7 +28,7 @@ export function QueueList({
   return (
     <>
       {tickets.map((t) => (
-        <QueueCard key={t.id} ticket={t} onComplete={() => onComplete(t.id)} onVoid={() => onVoid(t.id)} onAddItems={() => onAddItems(t.id)} />
+        <QueueCard key={t.id} ticket={t} onComplete={() => onComplete(t.id)} onVoid={() => onVoid(t.id)} onAddItems={() => onAddItems(t.id)} onAdvanceItem={onAdvanceItem} />
       ))}
     </>
   );
